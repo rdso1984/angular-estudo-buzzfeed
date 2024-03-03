d6a223313a1b046a6a0c9c37788334f916970225
rdso1984 <rdso1984@gmail.com>
2024-03-03 08:11:14 -0300

    Criada logica para montar perguntas e opcoes de resposta

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import quizz_questions from '../../../assets/data/quizz_questions.json'
 
 @Component({
   selector: 'app-quizz',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
 export class QuizzComponent {
 
+  title:string = ""
+
+  questions:any
+  questionSelected:any
+
+  answers:string[] = []
+  answerSelect:string = ""
+
+  questionIndex:number = 0
+  questionMaxIndex:number = 0
+
+  finished:boolean = false
+
+  ngOnInit():void {
+    if(quizz_questions){
+      this.finished = false
+      this.title = quizz_questions.title
+      this.questions = quizz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+    }
+  }
+
+  playerChoose(value:string) {
+    this.answers.push(value)
+  }
+
+  async nextStep() {
+    this.questionIndex+=1
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex]
+    } else {
+      this.finished = true
+      
+    }
+  }
 }
